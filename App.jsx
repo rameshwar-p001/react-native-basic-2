fd2653c22d1dccca4f1cd2d25c46b020f617cd9c
Patil Rameshwar <rameshwarr.p001@gmail.com>
2025-10-10 23:20:53 +0530

    day1

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import { Button, StyleSheet, Text} from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>hello how are you </Text>
+      <Button title='click here ' />
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
