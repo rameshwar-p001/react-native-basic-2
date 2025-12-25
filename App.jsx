004bb4040dfe1475f2900f85c9e615708052fb2f
Patil Rameshwar <rameshwarr.p001@gmail.com>
2025-12-25 19:30:54 +0530

    day 3

--- App.jsx
+++ App.jsx
@@ -21,17 +21,75 @@
 
 
 
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+// import { Text , StyleSheet, Button} from 'react-native'
+// import React from 'react'
+// import { SafeAreaView } from 'react-native-safe-area-context'
 
-const App = () => {
-  return (
-   <SafeAreaView>
+
+// const App = () => { 
     
-   </SafeAreaView>
-  )
-}
+//   return (
+//    <SafeAreaView>
+//       <Text style = {{ fontSize : 20 , marginTop : 20 ,color : "blue" }}>hello how are you </Text>    {/*here we give the inline css*/}
+//       <Text style={styles.text}>{count}</Text>           {/*here we give the Internal css*/}
+//       <Button title='Press here'/>
+//    </SafeAreaView>
+//   )
+// }
 
-export default App
+// export default App
+
+// // here we call the internal css 
+// const styles = StyleSheet.create({
+      
+//   text : {
+//     color : "red", 
+//     fontSize : 50,
+//     marginTop:20, 
+//   },
+// })
+
+
+// // // useState  
+// // with mini task 
+// import { Text, Button, View, StyleSheet, Pressable } from 'react-native';
+// import React, { useState } from 'react';
+
+// let App = () => {
+//   let [count, setCount] = useState(0);
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>The Count is : {count}</Text>
+//       <Pressable style={styles.bu}  title="On-Press" onPress={() => setCount(count + 1)} >  <Text style={styles.buttonText}>Increase</Text></Pressable>
+//       <Pressable style={styles.bu} title="for Minus" onPress={() => setCount(count - count)} >  <Text style={styles.buttonText}>Reset</Text></Pressable>
+//     </View>
+//   );
+// };
+
+// export default App
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   text: {
+//     fontSize: 30,
+//     marginBottom: 20,
+//   },
+//   bu:{
+//     backgroundColor: '#4CAF50',
+//     paddingVertical: 12,
+//     paddingHorizontal: 30,
+//     borderRadius: 8,
+//     marginVertical: 8
+//   },
+//   buttonText: {
+//     fontSize :40,
+//     alignItems: "center"
+//   }
+// });
+
 
